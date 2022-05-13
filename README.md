5312bac29b38a0a80b3eb2a3c13990dda785b774
Jiwoo Choi <jchoi136@gmail.com>
2022-05-13 18:52:57 +0900

    delete appending query function

--- README.md
+++ README.md
@@ -23,14 +23,3 @@ google extension to identify ads on search results
 Limitations/ tradeoffs : 
 
 If Google decides to use different tags other than span for ads, then this might not work. Also, if they change the wrapping elements to be more than the 6th parent node from the span, then it would not color the entire area of the ad. Additionally, Google labels ad areas with "Ad" or "Ads". "Ads" contains multiple ads in a carousel, but I counted these as only one ad.
-
-### Appending "in 2021" to all queries
-1. When active tab updates, check if query has been appended already.
-2. If not, append "in 2021" to query parameters by appending to the url.
-3. Send the appended url to `chome.tabs.update`.
-4. When the tab updates again and we see query parameter "in 2021" appended already, reload.
-5. After reloading page, inject the forground.
-
-Limitations/ tradeoffs :
-
-The page must load 2 additional times. The reload is necessary because the address bar updates, but a previous page is loaded sometimes for some reason. This is the only workaround I found using Manifest Version 3 (MV3). MV2 is deprecated, but would have allowed intercepting and adding query parameters to the request while preserving other query paramters using `chrome.webRequest` api. I decided against this since MV2 will not be supported in the future. MV3 has `chrome.declarativeNetRequest` which uses a json file only to modify query parameters. So, there is no way to save the previous query parameters and adding more query parameters overwrites the previous query parameters.
--- background.js
+++ background.js
@@ -1,13 +1,9 @@
-// Keeps track of reloads to avoid an infinite reload loop.
-var reloaded = false;
-
 chrome.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
-
   if (changeInfo.status != "complete") {
     return;
   }
 
-  let [tab1] = await chrome.tabs.query({ active: true, currentWindow: true });    
+  let [tab1] = await chrome.tabs.query({ active: true, currentWindow: true });
   let url = tab1.url;
 
   if (url.length < 1 || url === null || url === undefined) {
@@ -16,19 +12,7 @@ chrome.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
 
   // Check if user is on Google website.
   if (url.search("google.com/search?") > -1) {
-
-    // Check if 2021 is in url. Typically this means its a query parameter.
-    // TODO: Do a regex check for a more robust solution.
-    if (url.search("2021") > -1) {
-      chrome.storage.sync.get("reloadedVal", function(result){
-        reloaded = result.reloadedVal;
-        if (reloaded == false) {
-          reloaded = true;
-          chrome.storage.sync.set({"reloadedVal": true});
-          chrome.tabs.reload(tabId, {bypassCache: true});
-        }
-      });
-      chrome.scripting
+    chrome.scripting
       .executeScript({
         target: { tabId: tabId },
         files: ["./foreground.js"],
@@ -37,11 +21,5 @@ chrome.tabs.onUpdated.addListener(async (tabId, changeInfo, tab) => {
         console.log("INJECTED THE FOREGROUND SCRIPT.");
       })
       .catch((err) => console.log(err));
-    } else {
-      reloaded = false;
-      chrome.storage.sync.set({"reloadedVal": false});
-      url += "&q=in\+2021"
-      chrome.tabs.update(tabId, {url: url});
-    }
   }
 });
